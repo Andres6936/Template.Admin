0a49a4765a75fb1c657a1eaf7d442e45288b627e
Joan Andrés <andres6936@live.com>
2021-01-17 21:04:19 -0500

    Moved files and support to deliver html files

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`Executing app at http://localhost:${port}`);
+})
