7281f71c40a12934424897d28ea56541776b59ce
Joan Andrés <andres6936@live.com>
2021-01-17 21:44:15 -0500

    Moved the routes to another directory

--- index.js
+++ index.js
@@ -6,63 +6,63 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 })
 
 app.get('/index.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 })
 
 app.get('/buttons.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/buttons.html'));
+    res.sendFile(path.join(__dirname + '/public/html/buttons.html'));
 })
 
 app.get('/cards.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/cards.html'));
+    res.sendFile(path.join(__dirname + '/public/html/cards.html'));
 })
 
 app.get('/utilities-color.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/utilities-color.html'));
+    res.sendFile(path.join(__dirname + '/public/html/utilities-color.html'));
 })
 
 app.get('/utilities-border.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/utilities-border.html'));
+    res.sendFile(path.join(__dirname + '/public/html/utilities-border.html'));
 })
 
 app.get('/utilities-animation.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/utilities-animation.html'));
+    res.sendFile(path.join(__dirname + '/public/html/utilities-animation.html'));
 })
 
 app.get('/utilities-other.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/utilities-other.html'));
+    res.sendFile(path.join(__dirname + '/public/html/utilities-other.html'));
 })
 
 app.get('/login.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/login.html'));
+    res.sendFile(path.join(__dirname + '/public/html/login.html'));
 })
 
 app.get('/register.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/register.html'));
+    res.sendFile(path.join(__dirname + '/public/html/register.html'));
 })
 
 app.get('/forgot-password.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/forgot-password.html'));
+    res.sendFile(path.join(__dirname + '/public/html/forgot-password.html'));
 })
 
 app.get('/blank.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/blank.html'));
+    res.sendFile(path.join(__dirname + '/public/html/blank.html'));
 })
 
 app.get('/404.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/404.html'));
+    res.sendFile(path.join(__dirname + '/public/html/404.html'));
 })
 
 app.get('/charts.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/charts.html'));
+    res.sendFile(path.join(__dirname + '/public/html/charts.html'));
 })
 
 app.get('/tables.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/tables.html'));
+    res.sendFile(path.join(__dirname + '/public/html/tables.html'));
 })
 
 app.use(express.static('public'));
